3e3da5d399d63fa4853de552b5c5333c2fb559f3
aflinch <alexanderflinchum@gmail.com>
2026-02-06 21:55:46 -0600

    initial repository commit

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,27 @@
+export default function HeroSection() {
+    return (
+        <section id="heroSection" className="hero-section">
+            <div className="hero-section-content-box">
+                <div className="hero-section-content">
+                    <p className="hero-section-title">Hello, I'm Alex Flinchum</p>
+                    <h1 className="hero-section-title">
+                        <span className="hero-section-title-color">Full Stack</span>{" "}
+                        <br />
+                        Developer
+                    </h1>
+                    <p className="hero-section-description">
+                        Full-stack developer with 6+ years building, testing, and delivering production software.
+                        <br /> I hold a Master’s degree in Software Development from Boston University
+                        and an undergraduate degree in Informatics from Indiana University, providing a strong foundation
+                        in software engineering, systems thinking, and applied computing.
+                    </p>
+                </div>
+                <button className="blur btn btn-primary">Get In Touch</button>
+                <button className="blur btn btn-primary">Download CV</button>
+            </div>
+            <div className="hero-section-imgx">
+                <img src="./img/hero_img.png" alt="Hero Section Img"/>
+            </div>
+        </section>
+    )
+}
