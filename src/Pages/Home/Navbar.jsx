0f2abbe32a2807403dfb82fe42479c90f5ab70aa
aflinch <alexanderflinchum@gmail.com>
2026-02-08 21:14:53 -0600

    implemented handle resize

--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -2,5 +2,27 @@ import { useState, useEffect } from "react";
 import { Link } from "react-scroll";
 
 function Navbar() {
+    const [navActive, setNavActive] = useState(false);
 
+    const toggleNav = () => {
+        setNavActive(!navActive);
+    }
+
+    const closeMenu = () => {
+        setNavActive(false);
+    }
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth <= 500) {
+                closeMenu();
+            }
+        }
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        }
+    });
 }
