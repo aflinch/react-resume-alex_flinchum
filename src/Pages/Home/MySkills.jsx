ae3b7036c9c20d960b582a6a5ee761ae77525c54
aflinch <alexanderflinchum@gmail.com>
2026-02-12 21:43:01 -0600

    update to title in skills section

--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -4,7 +4,7 @@ export default function MySkills() {
     return (
         <section className="skills-section" id="mySkills">
             <div className="skills-contianer">
-                <p className="skills-section-title">My Skills</p>
+                <h3 className="skills-section-title">My Skills</h3>
                 <h2 className="skills-section-heading">My Expertise</h2>
             </div>
             <div className="skills-section-contianer">
