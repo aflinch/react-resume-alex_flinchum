1dbbc56248437710b12032e67c43f2d102cd00c0
aflinch <alexanderflinchum@gmail.com>
2026-02-16 21:29:56 -0600

    Setup click on skills section

--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -1,13 +1,17 @@
 import data from '../../data/index.json';
+import SkillsModel from '../../Components/SkillsModel.jsx';
+import React from "react";
 
 export default function MySkills() {
+    const [showModel, setShowModel] = useState(false);
+
     return (
         <section className="skills-section" id="mySkills">
             <div className="skills-contianer">
                 <h3 className="skills-section-title">Expertise</h3>
                 <h2 className="skills-section-heading">My <span className="skills-section-sub-color">Skills</span></h2>
             </div>
-            <button className="skills-section-container">
+            <button className="skills-section-container" onClick={() => setShowModel(true)}>
                 {data?.skills?.map((item, index) => (
                     <div className="skills-section-card" key={index}>
                         <div className="skills-section-img">
@@ -20,6 +24,7 @@ export default function MySkills() {
                     </div>
                 ))}
             </button>
+            {showModel && <SkillsModel />}
         </section>
     )
 }
