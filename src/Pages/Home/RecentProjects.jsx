2132b106f75258ff7738b31e2764892ea32dc28e
aflinch <alexanderflinchum@gmail.com>
2026-05-16 18:51:55 -0500

    Added RecentProjects content, updated structure, and initialized projects data file

--- src/Pages/Home/RecentProjects.jsx
+++ src/Pages/Home/RecentProjects.jsx
@@ -7,7 +7,15 @@ export default function MySkills() {
 
     return (
         <section className="projects-section" id="recentProjects">
-
+            <div className="projects-contianer">
+                <h2 className="projects-section-heading">Recent <span className="projects-section-sub-color">Projects</span></h2>
+                <p className="projects-section-description">
+                    Here are some of my most recent projects
+                    that highlight my experience in full-stack development and software engineering.
+                </p>
+            </div>
+            <div className="skills-section-container">
+            </div>
         </section>
     )
 }
