5a8490f782309315ae5816e5e79d44c551ab0fdc
aflinch <alexanderflinchum@gmail.com>
2026-06-03 15:11:56 -0500

    finished all content for contact page

--- src/Pages/Contact/ContactSection.jsx
+++ src/Pages/Contact/ContactSection.jsx
@@ -1,10 +1,38 @@
 import React from "react";
+import {Github, Linkedin, Mail, Phone} from "lucide-react";
 
 export default function ContactSection() {
 
     return (
         <section id="contactSection" className="contact-section">
+            <div className="contact-section-content">
+                <div className="contact-container">
+                    <div className="section-heading-row">
+                        <h2 className="contact-section-heading">CONTACT<span className="sub-color">.</span></h2>
+                        <hr className="title-divider" />
+                    </div>
+                    <h3 className="contact-section-title">Get in <span className="sub-color">Touch</span></h3>
+                </div>
 
+                <div className="contact-info-section">
+                    <h4>Contact Information</h4>
+                    <span className="contact-info">
+                        <Phone /> (317) 650-5565
+                    </span>
+                    <span className="contact-info">
+                        <Mail /> alexanderflinchum@gmail.com
+                    </span>
+
+                    <span className="hero-section-links">
+                    <a href="https://www.linkedin.com/in/alexflinchum/" className="link" target="_blank" rel="noopener noreferrer">
+                        <Linkedin />
+                    </a>
+                    <a href="https://github.com/aflinch" className="link" target="_blank" rel="noopener noreferrer">
+                        <Github />
+                    </a>
+                </span>
+                </div>
+            </div>
         </section>
     )
 }
